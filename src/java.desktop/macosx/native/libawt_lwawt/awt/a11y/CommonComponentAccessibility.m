d56532116637a4e4a372e6fcc88dcfc95f9f2b31
Autumn808 <AutumnCapasso@gmail.com>
2022-12-12 16:58:19 -0800

    Revert "backport bbb93ca3c80968d4f167bb4cd9e5d3e60e4a6a5b"
    This reverts commit ad75e6d32ca3757ac09d579278d54fc6dec11e6f.
    
    Signed-off-by: Autumn808 <AutumnCapasso@gmail.com>

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -46,11 +46,9 @@ static NSMutableDictionary * _Nullable rolesMap;
     /*
      * Here we should keep all the mapping between the accessibility roles and implementing classes
      */
-    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:3];
+    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:1];
 
     [rolesMap setObject:@"ButtonAccessibility" forKey:@"pushbutton"];
-    [rolesMap setObject:@"ImageAccessibility" forKey:@"icon"];
-    [rolesMap setObject:@"ImageAccessibility" forKey:@"desktopicon"];
 }
 
 /*
