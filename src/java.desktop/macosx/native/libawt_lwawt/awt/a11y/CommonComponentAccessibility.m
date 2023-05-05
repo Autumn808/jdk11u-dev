982ace45b3174733db7792d06732038e563b6c33
Autumn808 <AutumnCapasso@gmail.com>
2023-05-05 14:18:14 -0700

    fixed formating changes

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -50,6 +50,7 @@ static jobject sAccessibilityClass = NULL;
      */
     rolesMap = [[NSMutableDictionary alloc] initWithCapacity:35];
 
+
     [rolesMap setObject:@"ButtonAccessibility" forKey:@"pushbutton"];
     [rolesMap setObject:@"ImageAccessibility" forKey:@"icon"];
     [rolesMap setObject:@"ImageAccessibility" forKey:@"desktopicon"];
@@ -67,6 +68,7 @@ static jobject sAccessibilityClass = NULL;
     [rolesMap setObject:@"GroupAccessibility" forKey:@"internalframe"];
     [rolesMap setObject:@"GroupAccessibility" forKey:@"swingcomponent"];
     [rolesMap setObject:@"ToolbarAccessibility" forKey:@"toolbar"];
+
 
     /*
      * All the components below should be ignored by the accessibility subsystem,
